496aee370d35e9ceb991970d29c27ab993f62737
Giorgi Gobechia <82951209+giorgigobechia@users.noreply.github.com>
2023-09-14 14:52:14 +0400

    1024

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { useState, useEffect } from 'react';
+
 import Advertising from "@/common/components/__organisms/Advertising";
 import DaysForecast from "@/common/components/__organisms/DaysForecast";
 import TodaysHiglights from "@/common/components/__organisms/TodaysHiglights";
@@ -5,6 +8,26 @@ import WeatherToday from "@/common/components/__organisms/WeatherToday";
 import Image from "next/image";
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 1024);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  if (isMobile) {
+    return <p>MOBILE GOES HERE</p>;
+  }
+
   return (
     <main className="w-full">
       <article className="flex flex-col gap-3 w-full h-full">
