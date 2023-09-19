cd71cf18302f1ae8513439dad11437f860efe5bc
ugulavaleri <leri.ugulava1@gmail.com>
2023-09-19 17:21:18 +0400

    fix: fixed main page & sidebar colors

--- src/common/components/__organisms/WeatherToday.tsx
+++ src/common/components/__organisms/WeatherToday.tsx
@@ -19,133 +19,142 @@ import React, { useEffect, useState } from "react";
 import SearchBar from "../__molecules/SearchBar";
 
 interface PropsTypes {
-  currentTemp: number;
-  currentWeatherDesc: string;
-  month: string;
-  day: string;
-  min: number;
-  max: number;
+    currentTemp: number;
+    currentWeatherDesc: string;
+    month: string;
+    day: string;
+    min: number;
+    max: number;
 }
 
 const WeatherToday = ({
-  currentTemp,
-  currentWeatherDesc,
-  month,
-  day,
-  min,
-  max,
+    currentTemp,
+    currentWeatherDesc,
+    month,
+    day,
+    min,
+    max,
 }: PropsTypes) => {
-  const { resolvedTheme } = useTheme();
-  const { language } = useGlobalContext();
-  const [currentTime, setCurrentTime] = useState(new Date());
-  const dayNum = currentTime.getDate();
-  const [searchActive, setSearchActive] = useState(false);
-  const { activeCity } = useGlobalContext();
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 60000);
+    const { resolvedTheme } = useTheme();
+    const { language } = useGlobalContext();
+    const [currentTime, setCurrentTime] = useState(new Date());
+    const dayNum = currentTime.getDate();
+    const [searchActive, setSearchActive] = useState(false);
+    const { activeCity } = useGlobalContext();
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 60000);
 
-    return () => {
-      clearInterval(intervalId);
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    const formattedTime = currentTime.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+    });
+    const handleSearchClose = () => {
+        setSearchActive(false);
     };
-  }, []);
+    return (
+        <section className="md:p-4 xxl:p-7 flex flex-col justify-between  xxl:rounded-[34px] md:rounded-[26px] bg-[#cea9a927] dark:bg-[#355a716b] w-[30%] relative overflow-hidden">
+            {searchActive && (
+                <SearchBar handleSearchClose={handleSearchClose} />
+            )}
+            <div
+                className="absolute w-full h-full -z-10 "
+                style={{ backdropFilter: "blur(14px)" }}
+            ></div>
+            <div className="absolute w-full h-full -z-20">
+                <PinkGradient
+                    width={300}
+                    height={283}
+                    className="absolute xxl:left-10 "
+                />
+                <GrayGradient
+                    width={308}
+                    height={300}
+                    className="absolute xxl:right-6 xxl:top-[-50px] md:right-2 md:top-[-80px]"
+                />
+                <GreenGradient
+                    width={478}
+                    height={360}
+                    className="absolute xxl:bottom-10 xxl:left-0 md:bottom-[-50px] md:left-[-30px]"
+                />
+            </div>
+            <div className="flex w-full justify-between mt-4 ">
+                <h1 className="xxl:text-[40px] md:text-[30px]">
+                    {" "}
+                    {activeCity}
+                </h1>
+                <button
+                    className={`rounded-full xxl:py-2 xxl:px-3 md:p-2 bg-[#ffffff4d] dark:bg-[#0000004D] cursor-pointer ${
+                        searchActive && "hidden"
+                    }`}
+                    onClick={() => setSearchActive(true)}
+                >
+                    <SearchIcon
+                        width={0}
+                        height={0}
+                        className="md:w-[28px] md:h-[28px] xxl:w-[36px] xxl:h-[36px]"
+                        stroke={resolvedTheme === "light" ? "black" : "white"}
+                    />
+                </button>
+            </div>
+            <div className=" xxl:py-[3vh] flex w-full items-center  gap-[4%]">
+                <WeatherIcons iconName={currentWeatherDesc} IconSize={175} />
 
-  const formattedTime = currentTime.toLocaleTimeString([], {
-    hour: "2-digit",
-    minute: "2-digit",
-  });
-  const handleSearchClose = () => {
-    setSearchActive(false);
-  };
-  return (
-    <section className="md:p-4 xxl:p-7 flex flex-col justify-between  xxl:rounded-[34px] md:rounded-[26px] bg-[#cea9a927] dark:bg-[#355a716b] w-[30%] relative overflow-hidden">
-      {searchActive && <SearchBar handleSearchClose={handleSearchClose} />}
-      <div
-        className="absolute w-full h-full -z-10 "
-        style={{ backdropFilter: "blur(14px)" }}
-      ></div>
-      <div className="absolute w-full h-full -z-20">
-        <PinkGradient
-          width={300}
-          height={283}
-          className="absolute xxl:left-10 "
-        />
-        <GrayGradient
-          width={308}
-          height={300}
-          className="absolute xxl:right-6 xxl:top-[-50px] md:right-2 md:top-[-80px]"
-        />
-        <GreenGradient
-          width={478}
-          height={360}
-          className="absolute xxl:bottom-10 xxl:left-0 md:bottom-[-50px] md:left-[-30px]"
-        />
-      </div>
-      <div className="flex w-full justify-between mt-4 ">
-        <h1 className="xxl:text-[40px] md:text-[30px]"> {activeCity}</h1>
-        <button
-          className={`rounded-full xxl:py-2 xxl:px-3 md:p-2 bg-[#ffffff4d] dark:bg-[#0000004D] cursor-pointer ${
-            searchActive && "hidden"
-          }`}
-          onClick={() => setSearchActive(true)}
-        >
-          <SearchIcon
-            width={0}
-            height={0}
-            className="md:w-[28px] md:h-[28px] xxl:w-[36px] xxl:h-[36px]"
-            stroke={resolvedTheme === "light" ? "black" : "white"}
-          />
-        </button>
-      </div>
-      <div className=" xxl:py-[3vh] flex w-full items-center  gap-[4%]">
-        <WeatherIcons iconName={currentWeatherDesc} IconSize={175} />
-
-        <div className="w-[40%]">
-          <h3 className="xxl:text-[54px] md:text-[50px]  leading-tight">
-            {currentTemp}
-            <sup>&deg;</sup>C
-          </h3>
-          <p className="text-sm">
-            მინ:{min}° მაქს:{max}°
-          </p>
-          <br />
-          <p className="xxl:text-base md:text-sm tracking-tight text-justify">
-            {currentWeatherDesc}
-          </p>
-        </div>
-      </div>
-      <div className="py-[2vh] flex flex-col gap-2 border-t-2 border-t-gray-400">
-        <p className="flex items-center gap-1 xxl:text-base md:text-sm">
-          <LocationIcon
-            width={21}
-            height={21}
-            stroke={resolvedTheme === "light" ? "black" : "white"}
-          />{" "}
-          Tbilisi , Georgia
-        </p>
-        <p className="flex items-center gap-1 xxl:text-base md:text-sm justify-between ">
-          <div className="flex">
-            <EmptyCalendarIcon
-              width={21}
-              height={21}
-              stroke={resolvedTheme === "light" ? "black" : "white"}
-            />{" "}
-            <DayCases dayResponse={day} />
-            , <MonthCases monthResponse={month} />
-            <span className="pl-2">{dayNum}</span>
-            <span className="pl-2">
-              {" "}
-              <b className="xxl:text-base md:text-sm">{formattedTime}</b>
-            </span>
-          </div>
-          <Link className="underline" href={"/twentyfive-days"}>
-            25 დღის
-          </Link>
-        </p>
-      </div>
-    </section>
-  );
+                <div className="w-[40%]">
+                    <h3 className="xxl:text-[54px] md:text-[50px]  leading-tight">
+                        {currentTemp}
+                        <sup>&deg;</sup>C
+                    </h3>
+                    <p className="text-sm">
+                        მინ:{min}° მაქს:{max}°
+                    </p>
+                    <br />
+                    <p className="xxl:text-base md:text-sm tracking-tight text-justify">
+                        {currentWeatherDesc}
+                    </p>
+                </div>
+            </div>
+            <div className="py-[2vh] flex flex-col gap-2 border-t-2 border-t-gray-400">
+                <p className="flex items-center gap-1 xxl:text-base md:text-sm">
+                    <LocationIcon
+                        width={21}
+                        height={21}
+                        stroke={resolvedTheme === "light" ? "black" : "white"}
+                    />{" "}
+                    Tbilisi , Georgia
+                </p>
+                <p className="flex items-center gap-1 xxl:text-base md:text-sm justify-between ">
+                    <div className="flex">
+                        <EmptyCalendarIcon
+                            width={21}
+                            height={21}
+                            stroke={
+                                resolvedTheme === "light" ? "black" : "white"
+                            }
+                        />{" "}
+                        <DayCases dayResponse={day} />
+                        , <MonthCases monthResponse={month} />
+                        <span className="pl-2">{dayNum}</span>
+                        <span className="pl-2">
+                            {" "}
+                            <b className="xxl:text-base md:text-sm">
+                                {formattedTime}
+                            </b>
+                        </span>
+                    </div>
+                    <Link className="underline" href={"/twentyfive-days"}>
+                        25 დღის
+                    </Link>
+                </p>
+            </div>
+        </section>
+    );
 };
 
 export default WeatherToday;
---
+++ src/common/icons/24HourWhiteIcon.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { IconProps } from "./IconTypes";
+
+function WhiteIcon({ width, height, className }: IconProps) {
+    return (
+        <svg
+            width={width}
+            height={height}
+            viewBox="0 0 365 279"
+            fill="none"
+            className={className}
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <ellipse
+                cx="182.691"
+                cy="139.308"
+                rx="212.007"
+                ry="86.2249"
+                transform="rotate(34.3446 182.691 139.308)"
+                fill="url(#paint0_radial_59_2538)"
+                fill-opacity="0.5"
+            />
+            <defs>
+                <radialGradient
+                    id="paint0_radial_59_2538"
+                    cx="0"
+                    cy="0"
+                    r="1"
+                    gradientUnits="userSpaceOnUse"
+                    gradientTransform="translate(182.691 139.308) rotate(90) scale(86.2249 212.007)"
+                >
+                    <stop stop-color="white" stop-opacity="0.38" />
+                    <stop offset="1" stop-color="white" stop-opacity="0" />
+                </radialGradient>
+            </defs>
+        </svg>
+    );
+}
+
+export default WhiteIcon;
