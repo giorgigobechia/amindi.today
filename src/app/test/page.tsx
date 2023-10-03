62eca50de3a244bf515845a95375b75df4c7d72f
Giorgi Gobechia <82951209+giorgigobechia@users.noreply.github.com>
2023-10-03 15:32:37 +0400

    davamate gverdi

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const Test = () => {
+    return (
+        <section className="md:px-4 md:py-4 xxl:px-7 xxl:py-5 xxl:rounded-[34px] pb-[100px] lg:pb-0 md:rounded-[26px] bg-[#a7aec727] dark:bg-[#355a716b] w-full h-full relative overflow-hidden flex flex-col xxl:gap-24 md:gap-16">
+           testi
+        </section>
+    );
+};
+
+export default Test;
